851790ef023bb87d384d8cfbc031f74bdb77aae8
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-04-15 18:11:10 +0530

    object creation

--- fatarrow.js
+++ fatarrow.js
@@ -1,4 +1,15 @@
-const multiplication = (x,y) => {
-    return(x*y)
+// const multiplication = (x,y) => {
+//     return(x*y)
+// }
+// console.log(multiplication(4,8));
+
+// Task 2 - create a student object
+const student = {
+    name: "sam",
+    age:20,
+    branch:"science",
+    detail(){
+        console.log(`hi i am ${this.name}`)
+    }
 }
-console.log(multiplication(4,8));
+student.detail()
