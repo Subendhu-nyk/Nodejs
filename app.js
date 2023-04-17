49135eea855fa2f643d7333d7dfc176631073f14
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-04-17 10:37:58 +0530

    return a response

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const http= require('http');
-const server=http.createServer((req,res)=> {
-    console.log("Subendhu");
-})
-server.listen(4000)
+// const http= require('http');
+// const server=http.createServer((req,res)=> {
+//     console.log("Subendhu");
+// })
+// server.listen(4000)
 
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>My First Page</title><head>');
+res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+res.write('</html>');
+res.end();
+});
+server.listen(3000);
