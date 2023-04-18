31081d6b1153605b90ec13681bae9deac1890c36
Subendhu-nyk <subendhu.nyk@gmail.com>
2023-04-18 09:55:26 +0530

    export

--- rw.js
+++ rw.js
@@ -67,54 +67,16 @@
 // server.listen(3000);
 
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
-    res.write('<body>');
+const routes=require('./routes');
+// const fs = require('fs');
+console.log(routes.someText)
+const server = http.createServer(routes.handler);
 
-    // Read messages from file and display them
-    const messages = fs.readFileSync('message.txt', 'utf-8');
-    if (messages) {
-     
-      const messageList = messages.split('\n');
-      messageList.forEach(message => {
-        res.write(`${message}`);
-      });
-     
-    }
-
-    res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.appendFile('message.txt', message +"\n", err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');   
-        return res.end();
-      });
-    });
-   
-  }
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title><head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+// const server = http.createServer((req, res) => {
+//   // const url = req.url;
+//   // const method = req.method;
+  
+// });
 
 server.listen(3000);
